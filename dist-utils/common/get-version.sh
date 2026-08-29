7acc2b23c243dc08951a69d142dec73884b2c824
DirtyLesion <ryan66699986@googlemail.com>
2026-08-29 18:10:25 +0100

    Replace imported fork with pinned upstream project layout

--- dist-utils/common/get-version.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$( dirname "${BASH_SOURCE[0]}" )/../.." >/dev/null 2>&1 
-
-sed -E '/^project\(/!d;s/^.*VERSION "([^"]+)".*$/\1/g' CMakeLists.txt
--- distros/Ubuntu/mkconfig.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export COPY_CMD='cp stremio-shell/*.deb /app/'
-export CLEAN_CMD=
