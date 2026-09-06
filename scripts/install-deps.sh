74cbf11666cb13dfe2decc1283383c33b028c537
DirtyLesion <ryan66699986@googlemail.com>
2026-09-06 19:38:39 +0100

    Use current Stremio Linux shell build dependencies

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -11,31 +11,34 @@ sudo apt-get install -y \
   binutils \
   build-essential \
   ca-certificates \
-  cmake \
+  cargo \
   curl \
   debhelper \
   devscripts \
   dpkg-dev \
   fakeroot \
   file \
+  gettext \
   git \
+  libadwaita-1-dev \
   libasound2-dev \
   libass-dev \
   libdisplay-info-dev \
   libdrm-dev \
   libegl1-mesa-dev \
+  libepoxy-dev \
   libgbm-dev \
   libgnutls28-dev \
+  libgtk-4-dev \
   libjpeg-dev \
   liblcms2-dev \
   libpipewire-0.3-dev \
   libpulse-dev \
-  libqt5opengl5-dev \
-  libqt5webchannel5-dev \
   libssl-dev \
   libudev-dev \
   libvulkan-dev \
   libwayland-dev \
+  libwebkitgtk-6.0-dev \
   libx11-dev \
   libxext-dev \
   libxkbcommon-dev \
@@ -44,11 +47,10 @@ sudo apt-get install -y \
   libxss-dev \
   meson \
   ninja-build \
+  nodejs \
   patch \
   patchelf \
   pkgconf \
-  qtbase5-dev \
-  qtdeclarative5-dev \
-  qtwebengine5-dev \
+  rustc \
   wayland-protocols \
   zlib1g-dev
